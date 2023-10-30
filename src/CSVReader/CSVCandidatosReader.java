023d1975ad6e6adfd69635a1cefee8142afdd4dc
Edluck <Edlucastheotonio@gmail.com>
2023-10-29 23:07:35 -0300

    base6

--- src/CSVReader/CSVCandidatosReader.java
+++ src/CSVReader/CSVCandidatosReader.java
@@ -4,6 +4,14 @@ import java.io.FileNotFoundException;
 
 public class CSVCandidatosReader {
     public static void candidatosReader(String arquivo_cand) throws FileNotFoundException {
-        
+        try(java.util.Scanner scanner = new java.util.Scanner(new java.io.File(arquivo_cand))) {
+            scanner.nextLine();
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("Arquivo não encontrado");
+        }
+        catch(Exception e) {
+            System.out.println("Erro ao ler arquivo");
+        }
     }
 }
--- src/CSVReader/CSVVotacaoReader.java
+++ src/CSVReader/CSVVotacaoReader.java
@@ -4,6 +4,15 @@ import java.io.FileNotFoundException;
 
 public class CSVVotacaoReader {
     public static void votacaoReader(String arquivo_vot) throws FileNotFoundException {
-        
+        try(java.util.Scanner scanner = new java.util.Scanner(new java.io.File(arquivo_vot))) {
+            scanner.nextLine();
+            System.out.println(scanner.nextLine());
+        }
+        catch(FileNotFoundException e) {
+            System.out.println("Arquivo não encontrado");
+        }
+        catch(Exception e) {
+            System.out.println("Erro ao ler arquivo");
+        }
     }
 }
