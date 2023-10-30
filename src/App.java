29ee9cc8fd55050cd57ff3fda5f7165efbfdc76c
Edluck <Edlucastheotonio@gmail.com>
2023-10-29 21:46:25 -0300

    comecar leitura de aqruivo

--- src/App.java
+++ src/App.java
@@ -7,7 +7,6 @@ public class App {
     public static void main(String[] args) throws FileNotFoundException {
         CSVCandidatosReader.candidatosReader(args[1]);
         CSVVotacaoReader.votacaoReader(args[2]);
-        
-    
+ 
 }
 }
